e00d45a0eab8e68614f8422ecc223e779c9fd7d4
sionka5 <74147075+sionka5@users.noreply.github.com>
2022-03-02 15:47:18 +0100

    Add files via upload
    fixed install.sh script

---
+++ install.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Bash Script for install Fsociety tools
+# Must run to install tool
+
+clear
+echo "
+░██████╗██╗░█████╗░███╗░░██╗░██████╗░██╗░░░██╗███████╗
+██╔════╝██║██╔══██╗████╗░██║██╔════╝░██║░░░██║╚════██║
+╚█████╗░██║██║░░██║██╔██╗██║██║░░██╗░██║░░░██║░░███╔═╝
+░╚═══██╗██║██║░░██║██║╚████║██║░░╚██╗██║░░░██║██╔══╝░░
+██████╔╝██║╚█████╔╝██║░╚███║╚██████╔╝╚██████╔╝███████╗
+╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝░╚═════╝░░╚═════╝░╚══════╝
+
+██╗███╗░░██╗██████╗░██╗░░░██╗░██████╗████████╗██████╗░██╗███████╗░██████╗
+██║████╗░██║██╔══██╗██║░░░██║██╔════╝╚══██╔══╝██╔══██╗██║██╔════╝██╔════╝
+██║██╔██╗██║██║░░██║██║░░░██║╚█████╗░░░░██║░░░██████╔╝██║█████╗░░╚█████╗░
+██║██║╚████║██║░░██║██║░░░██║░╚═══██╗░░░██║░░░██╔══██╗██║██╔══╝░░░╚═══██╗
+██║██║░╚███║██████╔╝╚██████╔╝██████╔╝░░░██║░░░██║░░██║██║███████╗██████╔╝
+╚═╝╚═╝░░╚══╝╚═════╝░░╚═════╝░╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝╚══════╝╚═════╝░
+";
+
+
+if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
+    INSTALL_DIR="$PREFIX/usr/share/doc/anonfilesuploader"
+    BIN_DIR="$PREFIX/bin/"
+    BASH_PATH="$PREFIX/bin/bash"
+    TERMUX=true
+
+elif [ "$(uname)" = "Darwin" ]; then
+    INSTALL_DIR="/usr/local/anonfilesuploader"
+    BIN_DIR="/usr/local/bin/"
+    BASH_PATH="/bin/bash"
+    TERMUX=false
+else
+    INSTALL_DIR="$HOME/.anonfilesuploader"
+    BIN_DIR="/usr/local/bin/"
+    BASH_PATH="/bin/bash"
+    TERMUX=false
+
+fi
+
+echo "[✔] Checking directories...";
+if [ -d "$INSTALL_DIR" ]; then
+    echo "[◉] A directory anonfilesuploader was found! Do you want to replace it? [Y/n]:" ;
+    read -r mama
+    if [ "$mama" = "y" ]; then
+        if [ "$TERMUX" = true ]; then
+            rm -rf "$INSTALL_DIR"
+            rm "$BIN_DIR/anonfilesuploader*"
+        else
+            sudo rm -rf "$INSTALL_DIR"
+            sudo rm "$BIN_DIR/anonfilesuploader*"
+        fi
+    else
+        echo "[✘] If you want to install you must remove previous installations [✘] ";
+        echo "[✘] Installation failed! [✘] ";
+        exit
+    fi
+fi
+echo "[✔] Cleaning up old directories...";
+if [ -d "$ETC_DIR/Sionguz" ]; then
+    echo "$DIR_FOUND_TEXT"
+    if [ "$TERMUX" = true ]; then
+        rm -rf "$ETC_DIR/Sionguz"
+    else
+        sudo rm -rf "$ETC_DIR/Sionguz"
+    fi
+fi
+
+echo "[✔] Installing ...";
+echo "";
+git clone --depth=1 https://github.com/Sionka5/AnonfilesUploader "$INSTALL_DIR";
+echo "#!$BASH_PATH
+python $INSTALL_DIR/AnonUploader.py" "${1+"$@"}" > "$INSTALL_DIR/anonfilesuploader";
+chmod +x "$INSTALL_DIR/anonfilesuploader";
+if [ "$TERMUX" = true ]; then
+    cp "$INSTALL_DIR/anonfilesuploader" "$BIN_DIR"
+    
+else
+    sudo cp "$INSTALL_DIR/anonfilesuploader" "$BIN_DIR"
+    
+fi
+rm "$INSTALL_DIR/anonfilesuploader";
+
+
+if [ -d "$INSTALL_DIR" ] ;
+then
+    echo "";
+    echo "[✔] Tool installed successfully! [✔]";
+    echo "";
+    echo "[✔]====================================================================[✔]";
+    echo "[✔]    All is done!! You can execute tool by typing  ! anonuploader    [✔]";
+    echo "[✔]====================================================================[✔]";
+    echo "";
+else
+    echo "[✘] Installation failed! [✘] ";
+    exit
+fi
